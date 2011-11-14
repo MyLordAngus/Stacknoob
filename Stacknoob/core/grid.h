163c2cb8c2ecc2af7fd610fec0a6b5b7171c0a8b
flysteur <flysteur@gmail.com>
2011-11-14 13:13:13 +0100

    Merge conflict

--- Stacknoob/core/grid.h
+++ Stacknoob/core/grid.h
@@ -12,9 +12,11 @@ protected:
     int width;
     int height;
     vector< vector<Cell> > cells;
+
 public:
     Grid();
     vector< vector<Cell> > getCells() { return this->cells; }
+    Grid(int _width,int _height);
 };
 
 #endif // GRID_H
