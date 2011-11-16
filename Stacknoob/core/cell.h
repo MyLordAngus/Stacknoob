b0bd5ef3c4f3864f913977208bc4857e4209734f
Amaury Denoyelle <amaury.denoyelle@gmail.com>
2011-11-16 10:02:24 +0100

    Color cell on display
    Add method in Cell to retrieve a QColor object according to the
    attribute color
    Change the board::mergePieceInView. Now copy the cell in the merged
    board.

--- Stacknoob/core/cell.h
+++ Stacknoob/core/cell.h
@@ -3,7 +3,7 @@
 
 #include <QColor>
 
-enum colorType {BLACK, CYAN ,YELLOW ,MAGENTA ,ORANGE ,BLUE ,GREEN ,RED };
+enum colorType { BLACK, CYAN ,YELLOW ,MAGENTA ,ORANGE ,BLUE ,GREEN ,RED };
 
 class Cell
 {
@@ -14,9 +14,12 @@ protected:
 public:
     Cell();
     Cell(colorType _color, bool _blank);
+
     bool isBlank();
     void setIsBlank(bool b) { this->is_blank = b; }
-    void setColor(colorType _color) { this->color = _color; }
+    void setColor(colorType c) { this->color = c; }
+
+    /// Return the QColor object to paint it on view
     QColor getQColor();
 };
 
